184e44579dee294b7a812d8de5b8e06d120aaa4f
Scott H Lee <slee@logicfirstllc.com>
2020-07-26 23:10:29 +0000

    Automatically backed up by Learn

--- lib/04_fourth_challenge.rb
+++ lib/04_fourth_challenge.rb
@@ -5,7 +5,7 @@ def fourth_challenge
       :matriarch => {name: "Lady Montague", age: "54"},
       :hero => {name: "Romeo", age: "15", status: "alive"},
       :hero_friends => [
-        {name: "Benvolio", age: "17", attitude "worried",
+        {name: "Benvolio", age: "17", attitude "worried"},
         {name: "Mercutio", age: "18", attitude: "hot-headed"}
       ]
    }, 
@@ -18,7 +18,7 @@ def fourth_challenge
         {name: "Steven", age: "30", attitude: "confused"},
         {name: "Nurse", age: "44", attitude: "worried"}
       ]
-   }
-}
+    }
+ }
 
 end
